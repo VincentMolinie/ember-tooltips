71afaf49553dffb88b2219e48dbb8072e95d5c1b
Vincent Molinié <vincent.m@forestadmin.com>
2024-08-09 21:07:58 +0200

    fix: migrate to addon v2

---
+++ test-app/app/router.js
@@ -0,0 +1,15 @@
+import EmberRouter from '@ember/routing/router';
+import config from 'test-app/config/environment';
+
+const Router = EmberRouter.extend({
+  location: config.locationType,
+  rootURL: config.rootURL,
+});
+
+Router.map(function () {
+  this.route('acceptance');
+  this.route('many-tooltips');
+  this.route('update-for-reposition');
+});
+
+export default Router;
